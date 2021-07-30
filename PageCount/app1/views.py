75b8c48a93588e672c305c17f92445f6850e87ff
mohdabrar728 <mohdabrar728@gmail.com>
2021-07-30 23:04:04 +0530

    commit

--- PageCount/app1/views.py
+++ PageCount/app1/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def show(request):
+    var = request.session.get('count', 0)
+    counter = var + 1
+    request.session['count'] = counter
+    return render(request, 'base.html', {'counter': counter})
