f00d8f3cac5ef32ccd2da21a551850c5aece1137
mohdabrar728 <mohdabrar728@gmail.com>
2021-07-31 06:44:53 +0530

    commit

--- PageCount/PageCount/urls.py
+++ PageCount/PageCount/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app1.views import show
+from app1.views import show,stop
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show, name='show'),
+    path('stop',stop,name='stop'),
 ]
--- PageCount/app1/views.py
+++ PageCount/app1/views.py
@@ -6,4 +6,12 @@ def show(request):
     var = request.session.get('count', 0)
     counter = var + 1
     request.session['count'] = counter
-    return render(request, 'base.html', {'counter': counter})
+    return render(request, 'base.html', {'counter': counter, 'refresh': 'refresh',
+                                         'start': 'btn-light', 'stop': 'btn-outline-light'})
+
+
+def stop(request):
+    if 'count' in request.session:
+        del request.session['count']
+    return render(request, 'base.html', {'counter': 0, 'refresh': '',
+                                         'stop': 'btn-light', 'start': 'btn-outline-light'})
